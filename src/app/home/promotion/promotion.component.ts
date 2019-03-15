be31c7d65e023ccf373962f7588df4ea402347eb
Vikasini <vikasininithi@gmail.com>
2019-03-15 10:35:33 +0530

    changes in home page

--- src/app/home/promotion/promotion.component.ts
+++ src/app/home/promotion/promotion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Promotion } from './promotion.model';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-promotion',
@@ -8,9 +9,11 @@ import { Promotion } from './promotion.model';
 })
 export class PromotionComponent implements OnInit {
   @Input () promotion: Promotion;
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
   }
-
+  productView(data) {
+    this.router.navigate(['/product/productview', data._id]);
+  }
 }
